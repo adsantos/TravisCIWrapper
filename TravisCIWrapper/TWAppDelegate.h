9efeb8ecd647138001fb014daaaa03ec554fad80
Adriana Santos <asantos@yahoo-inc.com>
2013-08-06 14:51:55 +0100

    Initial Commit

---
+++ TravisCIWrapper/TWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  TWAppDelegate.h
+//  TravisCIWrapper
+//
+//  Created by Y.CORP.YAHOO.COM\asantos on 8/6/13.
+//  Copyright (c) 2013 asantos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TravisCIWrapper/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TravisCIWrapper
+//
+//  Created by Y.CORP.YAHOO.COM\asantos on 8/6/13.
+//  Copyright (c) 2013 asantos. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "TWAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([TWAppDelegate class]));
+    }
+}
